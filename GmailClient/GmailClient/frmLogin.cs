a1580743cda3f6950ff9703bdc0bbad6d0f0a3dc
SD8Z <sd8z@hotmail.com>
2017-01-09 21:45:24 +0100

    Login finalizados

--- GmailClient/GmailClient/frmLogin.cs
+++ GmailClient/GmailClient/frmLogin.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GmailQuickstart;
+using System.Threading;
 
 namespace GmailClient
 {
     public partial class frmLogin : Form
     {
+
         public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            bgwWait.RunWorkerAsync();
+        }
+
+        private void btnContinuar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void bgwWait_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Program.SetService(Login.DoLogin());
+        }
+
+        private void bgwWait_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btnContinuar.Enabled = true;
         }
     }
 }
