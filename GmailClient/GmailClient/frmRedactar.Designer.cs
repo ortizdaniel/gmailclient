a07236c5af2bb8c12f0bcb8e8631d39d40e369e1
Sergi <sergi.valbuena.2015@salleurl.edu>
2017-01-10 22:44:13 +0100

    Revert "Merge branch 'master' of https://bitbucket.org/dhilazo/gmailclient"
    This reverts commit d8124c201be1087da4fe1d32d72885eb7fc7e64a, reversing
    changes made to 9e6c760ebea6b6268d26ca1423651f31a435f65b.

--- GmailClient/GmailClient/frmRedactar.Designer.cs
+++ GmailClient/GmailClient/frmRedactar.Designer.cs
@@ -28,23 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRedactar));
             this.lblDestinatario = new System.Windows.Forms.Label();
             this.lblContenido = new System.Windows.Forms.Label();
+            this.tbContenido = new System.Windows.Forms.TextBox();
             this.lblAsunto = new System.Windows.Forms.Label();
             this.tbDestinatario = new System.Windows.Forms.TextBox();
             this.tbAsunto = new System.Windows.Forms.TextBox();
             this.cbSize = new System.Windows.Forms.ComboBox();
             this.cbFont = new System.Windows.Forms.ComboBox();
             this.btnBold = new System.Windows.Forms.Button();
-            this.btnUnderline = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.btnEnviar = new System.Windows.Forms.Button();
-            this.rtbContenido = new System.Windows.Forms.RichTextBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.imLSGmail = new System.Windows.Forms.PictureBox();
-            this.lbAdvice = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.imLSGmail)).BeginInit();
             this.SuspendLayout();
             // 
             // lblDestinatario
@@ -62,18 +56,27 @@
             // 
             this.lblContenido.AutoSize = true;
             this.lblContenido.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblContenido.Location = new System.Drawing.Point(12, 172);
+            this.lblContenido.Location = new System.Drawing.Point(12, 190);
             this.lblContenido.Name = "lblContenido";
             this.lblContenido.Size = new System.Drawing.Size(80, 18);
             this.lblContenido.TabIndex = 2;
             this.lblContenido.Text = "Contenido:";
             this.lblContenido.Click += new System.EventHandler(this.label2_Click);
             // 
+            // tbContenido
+            // 
+            this.tbContenido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbContenido.Location = new System.Drawing.Point(15, 211);
+            this.tbContenido.Multiline = true;
+            this.tbContenido.Name = "tbContenido";
+            this.tbContenido.Size = new System.Drawing.Size(448, 275);
+            this.tbContenido.TabIndex = 2;
+            // 
             // lblAsunto
             // 
             this.lblAsunto.AutoSize = true;
             this.lblAsunto.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAsunto.Location = new System.Drawing.Point(12, 123);
+            this.lblAsunto.Location = new System.Drawing.Point(12, 136);
             this.lblAsunto.Name = "lblAsunto";
             this.lblAsunto.Size = new System.Drawing.Size(58, 18);
             this.lblAsunto.TabIndex = 3;
@@ -91,7 +94,7 @@
             // tbAsunto
             // 
             this.tbAsunto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbAsunto.Location = new System.Drawing.Point(14, 144);
+            this.tbAsunto.Location = new System.Drawing.Point(15, 157);
             this.tbAsunto.Multiline = true;
             this.tbAsunto.Name = "tbAsunto";
             this.tbAsunto.Size = new System.Drawing.Size(448, 25);
@@ -109,11 +112,10 @@
             "12",
             "14",
             "16"});
-            this.cbSize.Location = new System.Drawing.Point(140, 3);
+            this.cbSize.Location = new System.Drawing.Point(142, 12);
             this.cbSize.Name = "cbSize";
             this.cbSize.Size = new System.Drawing.Size(40, 21);
             this.cbSize.TabIndex = 6;
-            this.cbSize.SelectedIndexChanged += new System.EventHandler(this.cbSize_SelectedIndexChanged);
             // 
             // cbFont
             // 
@@ -123,33 +125,30 @@
             "Calibri",
             "Franklin Gothic Book",
             "Microsoft Sans Serif"});
-            this.cbFont.Location = new System.Drawing.Point(3, 3);
+            this.cbFont.Location = new System.Drawing.Point(15, 12);
             this.cbFont.Name = "cbFont";
             this.cbFont.Size = new System.Drawing.Size(121, 21);
             this.cbFont.TabIndex = 7;
-            this.cbFont.SelectedIndexChanged += new System.EventHandler(this.cbFont_SelectedIndexChanged);
             // 
             // btnBold
             // 
             this.btnBold.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBold.Location = new System.Drawing.Point(192, 3);
+            this.btnBold.Location = new System.Drawing.Point(188, 8);
             this.btnBold.Name = "btnBold";
-            this.btnBold.Size = new System.Drawing.Size(28, 25);
+            this.btnBold.Size = new System.Drawing.Size(28, 26);
             this.btnBold.TabIndex = 8;
             this.btnBold.Text = "N";
             this.btnBold.UseVisualStyleBackColor = true;
-            this.btnBold.Click += new System.EventHandler(this.btnBold_Click);
             // 
-            // btnUnderline
+            // button1
             // 
-            this.btnUnderline.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUnderline.Location = new System.Drawing.Point(231, 3);
-            this.btnUnderline.Name = "btnUnderline";
-            this.btnUnderline.Size = new System.Drawing.Size(28, 25);
-            this.btnUnderline.TabIndex = 9;
-            this.btnUnderline.Text = "S";
-            this.btnUnderline.UseVisualStyleBackColor = true;
-            this.btnUnderline.Click += new System.EventHandler(this.btnUnderline_Click);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(222, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 26);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "S";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // btnEnviar
             // 
@@ -160,76 +159,26 @@
             this.btnEnviar.TabIndex = 10;
             this.btnEnviar.Text = "Enviar";
             this.btnEnviar.UseVisualStyleBackColor = false;
-            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
-            // 
-            // rtbContenido
-            // 
-            this.rtbContenido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtbContenido.Location = new System.Drawing.Point(12, 193);
-            this.rtbContenido.Name = "rtbContenido";
-            this.rtbContenido.Size = new System.Drawing.Size(448, 245);
-            this.rtbContenido.TabIndex = 11;
-            this.rtbContenido.Text = "";
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 5;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.48677F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.51323F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 39F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 42F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 176F));
-            this.tableLayoutPanel1.Controls.Add(this.cbFont, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.cbSize, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btnBold, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btnUnderline, 3, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(14, 441);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 68F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(447, 31);
-            this.tableLayoutPanel1.TabIndex = 13;
-            // 
-            // imLSGmail
-            // 
-            this.imLSGmail.Image = ((System.Drawing.Image)(resources.GetObject("imLSGmail.Image")));
-            this.imLSGmail.Location = new System.Drawing.Point(12, 12);
-            this.imLSGmail.Name = "imLSGmail";
-            this.imLSGmail.Size = new System.Drawing.Size(144, 58);
-            this.imLSGmail.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.imLSGmail.TabIndex = 10;
-            this.imLSGmail.TabStop = false;
-            // 
-            // lbAdvice
-            // 
-            this.lbAdvice.AutoSize = true;
-            this.lbAdvice.Location = new System.Drawing.Point(11, 476);
-            this.lbAdvice.Name = "lbAdvice";
-            this.lbAdvice.Size = new System.Drawing.Size(452, 13);
-            this.lbAdvice.TabIndex = 14;
-            this.lbAdvice.Text = "*Si se quiere utilizar diferentes tipos de letra se debe seleccionar el texto que" +
-    " se desea cambiar";
             // 
             // frmRedactar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(480, 498);
-            this.Controls.Add(this.lbAdvice);
-            this.Controls.Add(this.imLSGmail);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.rtbContenido);
             this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnBold);
+            this.Controls.Add(this.cbFont);
+            this.Controls.Add(this.cbSize);
             this.Controls.Add(this.tbAsunto);
             this.Controls.Add(this.tbDestinatario);
             this.Controls.Add(this.lblAsunto);
+            this.Controls.Add(this.tbContenido);
             this.Controls.Add(this.lblContenido);
             this.Controls.Add(this.lblDestinatario);
             this.Name = "frmRedactar";
             this.Text = "frmRedactar";
             this.Load += new System.EventHandler(this.frmRedactar_Load);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.imLSGmail)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -239,17 +188,14 @@
 
         private System.Windows.Forms.Label lblDestinatario;
         private System.Windows.Forms.Label lblContenido;
+        private System.Windows.Forms.TextBox tbContenido;
         private System.Windows.Forms.Label lblAsunto;
         private System.Windows.Forms.TextBox tbDestinatario;
         private System.Windows.Forms.TextBox tbAsunto;
         private System.Windows.Forms.ComboBox cbSize;
         private System.Windows.Forms.ComboBox cbFont;
         private System.Windows.Forms.Button btnBold;
-        private System.Windows.Forms.Button btnUnderline;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnEnviar;
-        private System.Windows.Forms.RichTextBox rtbContenido;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.PictureBox imLSGmail;
-        private System.Windows.Forms.Label lbAdvice;
     }
 }
--- GmailClient/GmailClient/frmRedactar.cs
+++ GmailClient/GmailClient/frmRedactar.cs
@@ -12,19 +12,11 @@ namespace GmailClient
 {
     public partial class frmRedactar : Form
     {
-        private Boolean bold = false;
-        private Boolean underline = false;
-        int size = 10;
-
         public frmRedactar()
         {
             InitializeComponent();
             cbFont.SelectedItem = "Microsoft Sans Serif";
             cbSize.SelectedItem = "10";
-            size = Int32.Parse(cbSize.SelectedItem.ToString());
-            Font font = new Font(cbFont.SelectedItem.ToString(), size);
-            tbAsunto.Font = font;
-            tbDestinatario.Font = font;
         }
 
         private void frmRedactar_Load(object sender, EventArgs e)
@@ -48,33 +40,6 @@ namespace GmailClient
 
         }
 
-        private void btnBold_Click(object sender, EventArgs e)
-        {
-            //Console.Write(tbContenido.SelectedText.ToString());
-            bold = !bold;
-            //if(richTextBox1.SelectionFont.Bold)
-            //if(tbContenido.SelectionFont.
-        }
 
-        private void btnUnderline_Click(object sender, EventArgs e)
-        {
-            underline = !underline;
-        }
-
-        private void cbSize_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            size = size = Int32.Parse(cbSize.SelectedItem.ToString());
-            rtbContenido.Font = new Font(rtbContenido.Font.Name,size);
-        }
-
-        private void cbFont_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            rtbContenido.Font = new Font(cbFont.SelectedItem.ToString(),size);
-        }
-
-        private void btnEnviar_Click(object sender, EventArgs e)
-        {
-            Console.WriteLine(rtbContenido.SelectedText);//testing
-        }
     }
 }
